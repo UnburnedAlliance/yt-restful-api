29a1ee06c6aa0df9ecf1920d5f2011e733185f4e
UnburnedAlliance <linenotsecure@gmail.com>
2023-02-22 11:08:00 -0600

    basic edits to user data for Aoo.js

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express();
 
 app.use((req, res, next) => {
     res.status(200).json({
-        message: 'It works'
+        message: 'Fuck you',
+        user_detail: {
+            last_used_wallet: "addr1q8ucfze9w0tf4chgrdr0rw382kwd8nj0n70nvp8jxs8qntalvkzqrtussxlqm4t89fz0h0es9u99hk22x8ggyf2yl38s63pr80",
+            first_used_date: "07/01/22",
+            displayname: "Seamoss Online",
+            website: "talismo.io",
+            shortbio: "Fuck Cinnamon Bun. Seriously, fuck that motherfucking cokehead piece of shit.",
+            profilepic: "https://randomuser.me/api/portraits/men/".concat((Math.floor(Math.random() * 100)),".jpg"),
+            twitterhandle: "wellspringagncy",
+            status: "REGISTERED",
+            status_date: "02/22/23"
+        }
     });
 });
 
